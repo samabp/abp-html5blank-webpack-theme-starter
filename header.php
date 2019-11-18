8c86bd79fdd5c013fbf840fd18d085d6b8a016bd
Sam Kobe <sam@anotherblankpage.com>
2019-11-18 11:08:37 -0800

    added keywords and open graph acf

--- header.php
+++ header.php
@@ -2,15 +2,19 @@
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="<?php bloginfo('description'); ?>" />
+		<meta name="keywords" content="<?php if ( have_rows( 'site_keywords', 'option' ) ) : ?><?php while ( have_rows( 'site_keywords', 'option' ) ) : the_row(); ?><?php the_sub_field( 'keywords' ); ?>, <?php endwhile; ?><?php else : ?>Keywords<?php endif; ?>" />
+		<meta property="og:title" content="<?php the_field( 'open_graph_title', 'option' ); ?>" />
+		<meta property="og:url" content="<?php the_field( 'open_graph_url', 'option' ); ?>" />
+		<meta property="og:image" content="<?php if ( get_field( 'open_graph_image', 'option' ) ) { the_field( 'open_graph_image', 'option' ); } ?>" />
+
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<link href="<?php if ( get_field( 'site_favicon', 'option' ) ) { the_field( 'site_favicon', 'option' ); } ?>" rel="shortcut icon">
+		<link href="<?php if ( get_field( 'site_apple_icon', 'option' ) ) { the_field( 'site_apple_icon', 'option' ); } ?>" rel="apple-touch-icon-precomposed">
 
 		<?php wp_head(); ?>
 		<script>
